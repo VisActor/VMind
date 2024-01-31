7ecdfbc36c1f56cc8567706774c5db6dbda46554
da730 <sduchd@gmail.com>
2024-01-31 19:58:18 +0800

    feat: add doc page

---
+++ docs/src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './app';
+
+import '@arco-design/web-react/dist/css/arco.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
