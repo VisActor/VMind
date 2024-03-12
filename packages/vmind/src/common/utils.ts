b7ba5c6ad006fdd4d92c18fbe7bc47f7fcb44c68
da730 <sduchd@gmail.com>
2024-03-12 21:29:55 +0800

    feat: usage statistics for skylark

--- packages/vmind/src/common/utils.ts
+++ packages/vmind/src/common/utils.ts
@@ -1,9 +1,13 @@
 export const calculateTokenUsage = (usageList: any[]) => {
-  let totalUsage = 0;
+  const totalUsage = {
+    completion_tokens: 0,
+    prompt_tokens: 0,
+    total_tokens: 0
+  };
   usageList.filter(Boolean).forEach(usage => {
-    //totalUsage += usage['prompt_tokens'] ?? 0
-    //totalUsage += usage['completion_tokens'] ?? 0
-    totalUsage += usage['total_tokens'] ?? 0;
+    totalUsage['completion_tokens'] += usage['completion_tokens'] ?? 0;
+    totalUsage['prompt_tokens'] += usage['prompt_tokens'] ?? 0;
+    totalUsage['total_tokens'] += usage['total_tokens'] ?? 0;
   });
   return totalUsage;
 };
