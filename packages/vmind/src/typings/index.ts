832b04cf54856b43cd806721c6c29dd020e10d74
da730 <sduchd@gmail.com>
2024-03-21 20:49:28 +0800

    feat: add patch pipelines for skylark

--- packages/vmind/src/typings/index.ts
+++ packages/vmind/src/typings/index.ts
@@ -39,7 +39,7 @@ export type GPTDataProcessResult = {
 export type Cell = {
   //字段映射，可用的视觉通道：["x","y","color","size","angle","time"]
   x?: string;
-  y?: string;
+  y?: string | string[];
   color?: string;
   size?: string;
   angle?: string;
@@ -78,7 +78,6 @@ export type Context = {
   colors?: string[];
   totalTime?: number;
 };
-export type Pipe = (src: any, context: Context) => any;
 
 export type TimeType = {
   totalTime: number;
