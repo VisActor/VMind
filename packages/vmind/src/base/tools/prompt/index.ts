530a539d3e013fd1135abe4a39bb19487f4229dc
da730 <sduchd@gmail.com>
2024-04-16 21:22:44 +0800

    feat: add comments

--- packages/vmind/src/base/tools/prompt/index.ts
+++ packages/vmind/src/base/tools/prompt/index.ts
@@ -2,7 +2,8 @@ import { IPrompt } from './types';
 /**
  * Prompt represents a LLM Prompt
  * Pass in Template when initialization
- * getPrompt method generates specific Prompt according to Template and Context
+ * getSystemPrompt method generates specific system Prompt according to Template and Context
+ * getUserPrompt method generate a user message that is used to request the LLM according to Context
  * The subclass needs to rewrite the getSystemPrompt and getUserPrompt method to generate a specific Prompt
  */
 export class Prompt<Context> implements IPrompt<Context> {
