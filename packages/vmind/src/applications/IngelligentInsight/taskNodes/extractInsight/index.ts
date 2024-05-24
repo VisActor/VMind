021aa722de6dd419b706cab528eaec39ffe21ddf
Darkside <sduchd@gmail.com>
2024-05-24 17:05:40 +0800

    feat: execute insight algorithms framework

--- packages/vmind/src/applications/IngelligentInsight/taskNodes/extractInsight/index.ts
+++ packages/vmind/src/applications/IngelligentInsight/taskNodes/extractInsight/index.ts
@@ -1,13 +1,32 @@
 import type { RuleBasedTaskNodeMeta } from '../../../../base/metaTypes';
 import { TaskNodeType } from '../../../../base/taskNode/types';
 import type { InsightContext, InsightOutput } from '../../../types';
-import type { ExtractInsightOutput } from '../../types';
+import type { DataProcessOutput, ExtractInsightOutput, VMindInsight } from '../../types';
+import type { Transformer } from '../../../../base/tools/transformer';
+import { pick } from '@visactor/vutils';
+import defaultInsightAlgorithms from './algorithms/default';
 
-//const executeInsightAlgo: Transformer<InsightContext, ExtractInsightOutput> = (context: InsightContext) => {};
+const executeInsightAlgo: Transformer<InsightContext & DataProcessOutput, ExtractInsightOutput> = (
+  context: InsightContext & DataProcessOutput
+) => {
+  const { insightAlgorithms = defaultInsightAlgorithms, chartType } = context;
+  const insights: VMindInsight[] = [];
+  const insightAlgorithmContext = { ...pick(context, ['chartType', 'dataset', 'fieldInfo', 'cell']), insights };
 
-const DataProcessTaskNodeMeta: RuleBasedTaskNodeMeta<InsightContext, InsightOutput> = {
-  type: TaskNodeType.RULE_BASED,
-  pipelines: []
+  insightAlgorithms.forEach(algoInfo => {
+    const { chartType: algoSupportedChartType, algorithmFunction } = algoInfo;
+    if (algoSupportedChartType.includes(chartType)) {
+      const res = algorithmFunction(insightAlgorithmContext);
+      insights.push(res);
+    }
+  });
+
+  return { insights };
 };
 
-export default DataProcessTaskNodeMeta;
+const ExtractInsightTaskNodeMeta: RuleBasedTaskNodeMeta<InsightContext & DataProcessOutput, InsightOutput> = {
+  type: TaskNodeType.RULE_BASED,
+  pipelines: [executeInsightAlgo]
+};
+
+export default ExtractInsightTaskNodeMeta;
