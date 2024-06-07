4c594b50e52703fc84a8351fcd9b8cf82b9134ee
Darkside <sduchd@gmail.com>
2024-06-07 15:53:17 +0800

    fix: build error

--- packages/vmind/src/applications/IngelligentInsight/taskNodes/extractInsight/algorithms/default.ts
+++ packages/vmind/src/applications/IngelligentInsight/taskNodes/extractInsight/algorithms/default.ts
@@ -1,37 +1,6 @@
 import { ChartType } from '../../../../../common/typings';
 import { InsightType, type InsightAlgorithm } from '../../../types';
 
-const defaultInsightAlgorithms: InsightAlgorithm[] = [
-  {
-    name: 'test',
-    chartType: [ChartType.BarChart, ChartType.LineChart],
-    insightType: InsightType.Outlier,
-    algorithmFunction: (context: any) => {
-      return [
-        {
-          type: InsightType.Outlier, // Insight type
-          data: [],
-          text: [],
-          significant: 0.1
-        }
-      ];
-    }
-  },
-  {
-    name: 'test2',
-    chartType: [ChartType.BarChart, ChartType.LineChart],
-    insightType: InsightType.ExtremeValue,
-    algorithmFunction: (context: any) => {
-      return [
-        {
-          type: InsightType.ExtremeValue, // Insight type
-          data: [],
-          text: [],
-          significant: 0.2
-        }
-      ];
-    }
-  }
-];
+const defaultInsightAlgorithms: InsightAlgorithm[] = [];
 
 export default defaultInsightAlgorithms;
