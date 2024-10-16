a3ecec7913657297b22bce5147d191359ea7d1b3
ZJU_czx <952370295@qq.com>
2024-10-16 14:58:28 +0800

    feat: update utils function

--- packages/vmind/src/atom/dataQuery/utils.ts
+++ packages/vmind/src/atom/dataQuery/utils.ts
@@ -3,11 +3,9 @@ import { isArray, isString, uniqArray } from '@visactor/vutils';
 import { capitalize, replaceAll } from '../../utils/text';
 import { alasqlKeywordList } from './const';
 import type { DataItem, DataTable, FieldInfo } from '../../types';
-import { DataType, ROLE } from '../../types';
-import dayjs from 'dayjs';
+import { ROLE } from '../../types';
 import alasql from 'alasql';
-import { isInteger } from '@visactor/calculator';
-import { sampleSize } from '../../utils/common';
+import { detectFieldType } from '../../utils/field';
 
 export function generateRandomString(len: number) {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -53,101 +51,9 @@ export const parseSQLResponse = (response: string) => {
   };
 };
 
-function validateDate(date: any) {
-  //check if the string is a data string
-  //only support YYYY-MM-DD and MM-DD
-  return dayjs(date, 'YYYY-MM-DD').isValid() || dayjs(date, 'MM-DD').isValid();
-}
-
 export function removeEmptyLines(str: string) {
   return str.replace(/\n\s*\n/g, '\n');
 }
-
-export const getFieldDomain = (dataset: DataItem[], column: string, role: ROLE) => {
-  //calculate domain of the column
-  const domain: (string | number)[] = dataset.map(d => (role === ROLE.DIMENSION ? d[column] : Number(d[column])));
-  return role === ROLE.DIMENSION
-    ? (uniqArray(domain) as string[]).slice(0, 20)
-    : [Math.min(...(domain as number[])), Math.max(...(domain as number[]))];
-};
-
-export const detectFieldType = (dataset: DataItem[], column: string): FieldInfo => {
-  let fieldType: DataType | undefined;
-  //detect field type based on rules
-  //The data types have the following inclusion relationships:
-  //date=>string
-  //int=>float=>string
-  //detect field type from strict to loose
-
-  dataset.every(data => {
-    const value = data[column];
-    const numberValue = Number(value);
-    if (!fieldType) {
-      //no accurate fieldType at the beginning, make the first one as fieldType
-      if (!isNaN(numberValue)) {
-        if (isInteger(numberValue)) {
-          fieldType = DataType.INT;
-        } else {
-          fieldType = DataType.FLOAT;
-        }
-      } else if (validateDate(value)) {
-        //check if the value is date
-        fieldType = DataType.DATE;
-      } else {
-        fieldType = DataType.STRING;
-      }
-      return true;
-    }
-    //already has a fieldType, check consistency
-    if (fieldType === DataType.DATE && !validateDate(value)) {
-      //current value is not date, field is string type
-      fieldType = DataType.STRING;
-      return false;
-    }
-    if (fieldType === DataType.INT) {
-      if (isNaN(numberValue)) {
-        //current value is not number, field is string type
-        fieldType = DataType.STRING;
-        return false;
-      } else if (!isInteger(numberValue)) {
-        //current value is not int, convert to float type and continue checking
-        fieldType = DataType.FLOAT;
-        return true;
-      }
-      return true;
-    }
-    if (fieldType === DataType.FLOAT) {
-      if (isNaN(numberValue)) {
-        //current value is not number, field is string type
-        fieldType = DataType.STRING;
-        return false;
-      }
-      return true;
-    }
-    if (fieldType === DataType.STRING) {
-      //no need to detect.
-      return false;
-    }
-    return true;
-  });
-  const role = [DataType.STRING, DataType.DATE].includes(fieldType) ? ROLE.DIMENSION : ROLE.MEASURE;
-  const domain = getFieldDomain(dataset, column, role);
-
-  return {
-    fieldName: column,
-    type: fieldType,
-    role,
-    domain
-  };
-};
-export const getFieldInfo = (dataset: DataItem[], columns: string[]): FieldInfo[] => {
-  let sampledDataset = dataset;
-  if (dataset.length > 1000) {
-    //sample the dataset if too large
-    sampledDataset = sampleSize(dataset, 1000);
-  }
-  return columns.map(column => detectFieldType(sampledDataset, column));
-};
 
 export const swapMap = (map: Map<string, string>) => {
   //swap the map
