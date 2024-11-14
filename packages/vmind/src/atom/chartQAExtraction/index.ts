1842e0688efef997c499c83fd43b62e3add63ee4
ZJU_czx <952370295@qq.com>
2024-11-14 16:00:39 +0800

    feat: add qa extraction of vchart faq

---
+++ packages/vmind/src/atom/chartQAExtraction/index.ts
@@ -0,0 +1,57 @@
+import { AtomName } from '../../types/atom';
+import type { BaseOptions } from '../type';
+import { BaseAtom } from '../base';
+import type { LLMMessage } from '../../types/llm';
+import type { ChartQAExtractionCtx } from '../../types';
+import { getSystemPrompt } from './prompt';
+import { parseLLMJson } from '../../utils/json';
+
+export class ChartQAExtraction extends BaseAtom<ChartQAExtractionCtx, BaseOptions> {
+  name = AtomName.CHART_QA_EXTRACTION;
+
+  isLLMAtom = true;
+
+  constructor(context: ChartQAExtractionCtx, option: BaseOptions) {
+    super(context, option);
+  }
+
+  getLLMMessages(query?: string): LLMMessage[] {
+    const { text } = this.context;
+    return [
+      {
+        role: 'system',
+        content: getSystemPrompt(this.options.language)
+      },
+      {
+        role: 'user',
+        content: text
+      }
+    ];
+  }
+
+  parseLLMContent(resJson: any): ChartQAExtractionCtx {
+    const { answerDSL, keyList, explanation } = resJson;
+    if (!answerDSL) {
+      console.error('Answer is Empty');
+      return {
+        ...this.context,
+        error: 'Answer is Empty'
+      };
+    }
+    const jsonRes = parseLLMJson(answerDSL);
+    if (!jsonRes.error) {
+      return {
+        ...this.context,
+        keyList,
+        explanation,
+        answer: jsonRes
+      };
+    }
+    return {
+      ...this.context,
+      keyList,
+      explanation,
+      answer: answerDSL
+    };
+  }
+}
