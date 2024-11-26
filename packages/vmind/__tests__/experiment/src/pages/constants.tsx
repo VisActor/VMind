4380deba9053c562f02215ddb52d270c5ecde7f5
ZJU_czx <952370295@qq.com>
2024-11-26 15:52:50 +0800

    chore: release beta version for vmind

--- packages/vmind/__tests__/experiment/src/pages/constants.tsx
+++ packages/vmind/__tests__/experiment/src/pages/constants.tsx
@@ -6,6 +6,7 @@ import { ChartGenerationTask } from './ChartGenerator/test';
 import { ChartGeneratorResult } from './ChartGenerator/caseStudy';
 import { ChartQAGenerator } from './ChartDialogueQA/test';
 import { QARag } from './ChartDialogueQA/qaPairRag';
+import { ColorPalette } from './ChartDialogueQA/tempColor';
 type MenuInfo = {
   menuItem: string;
   subItems: {
@@ -71,6 +72,11 @@ export const PLAYGROUND_MENU_INFO: {
         key: '1',
         name: 'QA Rag',
         component: <QARag />
+      },
+      {
+        key: '2',
+        name: 'Color Palette Test',
+        component: <ColorPalette />
       }
     ]
   }
