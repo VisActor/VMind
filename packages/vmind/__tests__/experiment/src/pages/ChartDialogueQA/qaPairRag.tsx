55f7550595e622c161a4409740c1c7b12b10db7b
ZJU_czx <952370295@qq.com>
2024-11-26 15:52:50 +0800

    chore: release beta version for vmind

--- packages/vmind/__tests__/experiment/src/pages/ChartDialogueQA/qaPairRag.tsx
+++ packages/vmind/__tests__/experiment/src/pages/ChartDialogueQA/qaPairRag.tsx
@@ -1,280 +1,66 @@
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import React from 'react';
-import { RAGManage } from '../../../../../src/index';
+import { CustomPrompt, LLMManage, Model, RAGManage } from '../../../../../src/index';
 import { Button, Input, Card } from '@arco-design/web-react';
-import { isString } from '@visactor/vutils';
+import { knowledge } from './const';
+import { isString, uniqArray } from '@visactor/vutils';
 import './rag.scss';
 const TextArea = Input.TextArea;
 
 const globalVariables = (import.meta as any).env;
 const qaRag = new RAGManage({
-  url: 'http://localhost:3333/api/viking_db/',
+  // url: 'http://localhost:3333/api/viking_db/',
+  url: 'http://10.90.34.164:3333/api/viking_db/',
   headers: {
     region: globalVariables.VITE_VIKING_DB_REGION,
     token: globalVariables.VITE_VIKING_DB_TOKEN
   },
   vikingdbName: globalVariables.VITE_VIKING_DB_NAME
 });
-const docsRag = new RAGManage({
-  url: 'http://localhost:3333/api/viking_db/',
+const keyPathRag = new RAGManage({
+  // url: 'http://localhost:3333/api/viking_db/',
+  url: 'http://10.90.34.164:3333/api/viking_db/',
   headers: {
     region: globalVariables.VITE_VIKING_DB_REGION,
-    token: globalVariables.VITE_VIKING_DB_DOCS_TOKEN
+    token: globalVariables.VITE_VIKING_DB_KEY_PATH_TOKEN
   },
-  vikingdbName: globalVariables.VITE_VIKING_DB_DOCS_NAME
+  vikingdbName: globalVariables.VITE_VIKING_DB_KEY_PATH_NAME
 });
+const llm = new LLMManage({
+  url: globalVariables.VITE_GPT_URL,
+  headers: {
+    'api-key': globalVariables.VITE_GPT_KEY,
+    Authorization: `Bearer ${globalVariables.VITE_GPT_KEY}`
+  },
+  maxTokens: 2048,
+  model: Model.GPT_4o
+});
+const topKeyAtom = new CustomPrompt({} as any, {
+  llm,
+  promptTemplate: `# Background
+You are a data visualization expert with an in-depth understanding of graphic grammar and visualization chart libraries such as d3, echarts, and ggplot2. You know how to use simple and straightforward language to help users understand visualization content. Now I have a new visualization frontend library called VChart. I will provide you with some knowledge content from VChart, and your task is to answer users' questions based on this knowledge content.
+# Knowledge
+\`\`\`
+${JSON.stringify(knowledge)}
+\`\`\`
+# Requirement
+1. Output the key value most relevant to the user's question.
+2. Only need to output the key value.
 
-const chartFileMapping = {
-  areaChart: [
-    'option.areaChart-axes-band.json',
-    'option.areaChart-axes-linear.json',
-    'option.areaChart-axes-log.json',
-    'option.areaChart-axes-symlog.json',
-    'option.areaChart-axes-time.json',
-    'option.areaChart-legends-color.json',
-    'option.areaChart-legends-discrete.json',
-    'option.areaChart-legends-size.json'
-  ],
-  bar3dChart: [
-    'option.bar3dChart-axes-band.json',
-    'option.bar3dChart-axes-linear.json',
-    'option.bar3dChart-axes-log.json',
-    'option.bar3dChart-axes-symlog.json',
-    'option.bar3dChart-axes-time.json',
-    'option.bar3dChart-legends-color.json',
-    'option.bar3dChart-legends-discrete.json',
-    'option.bar3dChart-legends-size.json'
-  ],
-  barChart: [
-    'option.barChart-axes-band.json',
-    'option.barChart-axes-linear.json',
-    'option.barChart-axes-log.json',
-    'option.barChart-axes-symlog.json',
-    'option.barChart-axes-time.json',
-    'option.barChart-legends-color.json',
-    'option.barChart-legends-discrete.json',
-    'option.barChart-legends-size.json'
-  ],
-  boxPlotChart: [
-    'option.boxPlotChart-axes-band.json',
-    'option.boxPlotChart-axes-linear.json',
-    'option.boxPlotChart-axes-log.json',
-    'option.boxPlotChart-axes-symlog.json',
-    'option.boxPlotChart-axes-time.json',
-    'option.boxPlotChart-legends-color.json',
-    'option.boxPlotChart-legends-discrete.json',
-    'option.boxPlotChart-legends-size.json'
-  ],
-  circlePackingChart: [
-    'option.circlePackingChart-legends-color.json',
-    'option.circlePackingChart-legends-discrete.json',
-    'option.circlePackingChart-legends-size.json'
-  ],
-  circularProgressChart: [
-    'option.circularProgressChart-axes-band.json',
-    'option.circularProgressChart-axes-linear.json',
-    'option.circularProgressChart-legends-color.json',
-    'option.circularProgressChart-legends-discrete.json',
-    'option.circularProgressChart-legends-size.json'
-  ],
-  commonChart: [
-    'option.commonChart-axes-band.json',
-    'option.commonChart-axes-linear.json',
-    'option.commonChart-axes-log.json',
-    'option.commonChart-axes-symlog.json',
-    'option.commonChart-axes-time.json',
-    'option.commonChart-legends-color.json',
-    'option.commonChart-legends-discrete.json',
-    'option.commonChart-legends-size.json',
-    'option.commonChart-series-area.json',
-    'option.commonChart-series-bar.json',
-    'option.commonChart-series-boxPlot.json',
-    'option.commonChart-series-circlePacking.json',
-    'option.commonChart-series-dot.json',
-    'option.commonChart-series-funnel.json',
-    'option.commonChart-series-heatmap.json',
-    'option.commonChart-series-line.json',
-    'option.commonChart-series-linearProgress.json',
-    'option.commonChart-series-link.json',
-    'option.commonChart-series-map.json',
-    'option.commonChart-series-radar.json',
-    'option.commonChart-series-rangeArea.json',
-    'option.commonChart-series-rangeColumn.json',
-    'option.commonChart-series-rose.json',
-    'option.commonChart-series-sankey.json',
-    'option.commonChart-series-scatter.json',
-    'option.commonChart-series-sunburst.json',
-    'option.commonChart-series-treemap.json',
-    'option.commonChart-series-waterfall.json',
-    'option.commonChart-series-wordCloud.json'
-  ],
-  correlationChart: [
-    'option.correlationChart-axes-band.json',
-    'option.correlationChart-axes-linear.json',
-    'option.correlationChart-legends-color.json',
-    'option.correlationChart-legends-discrete.json',
-    'option.correlationChart-legends-size.json'
-  ],
-  funnelChart: [
-    'option.funnelChart-legends-color.json',
-    'option.funnelChart-legends-discrete.json',
-    'option.funnelChart-legends-size.json'
-  ],
-  gaugeChart: [
-    'option.gaugeChart-axes-band.json',
-    'option.gaugeChart-axes-linear.json',
-    'option.gaugeChart-legends-color.json',
-    'option.gaugeChart-legends-discrete.json',
-    'option.gaugeChart-legends-size.json'
-  ],
-  heatmapChart: [
-    'option.heatmapChart-axes-band.json',
-    'option.heatmapChart-axes-linear.json',
-    'option.heatmapChart-axes-log.json',
-    'option.heatmapChart-axes-symlog.json',
-    'option.heatmapChart-axes-time.json',
-    'option.heatmapChart-legends-color.json',
-    'option.heatmapChart-legends-discrete.json',
-    'option.heatmapChart-legends-size.json'
-  ],
-  histogramChart: [
-    'option.histogramChart-axes-linear.json',
-    'option.histogramChart-axes-log.json',
-    'option.histogramChart-axes-symlog.json',
-    'option.histogramChart-axes-time.json',
-    'option.histogramChart-legends-color.json',
-    'option.histogramChart-legends-discrete.json',
-    'option.histogramChart-legends-size.json'
-  ],
-  lineChart: [
-    'option.lineChart-axes-band.json',
-    'option.lineChart-axes-linear.json',
-    'option.lineChart-axes-log.json',
-    'option.lineChart-axes-symlog.json',
-    'option.lineChart-axes-time.json',
-    'option.lineChart-legends-color.json',
-    'option.lineChart-legends-discrete.json',
-    'option.lineChart-legends-size.json'
-  ],
-  linearProgressChart: [
-    'option.linearProgressChart-axes-band.json',
-    'option.linearProgressChart-axes-linear.json',
-    'option.linearProgressChart-axes-log.json',
-    'option.linearProgressChart-axes-symlog.json',
-    'option.linearProgressChart-axes-time.json',
-    'option.linearProgressChart-legends-color.json',
-    'option.linearProgressChart-legends-discrete.json',
-    'option.linearProgressChart-legends-size.json'
-  ],
-  liquidChart: [
-    'option.liquidChart-legends-color.json',
-    'option.liquidChart-legends-discrete.json',
-    'option.liquidChart-legends-size.json'
-  ],
-  mapChart: [
-    'option.mapChart-legends-color.json',
-    'option.mapChart-legends-discrete.json',
-    'option.mapChart-legends-size.json'
-  ],
-  pieChart: [
-    'option.pieChart-axes-band.json',
-    'option.pieChart-axes-linear.json',
-    'option.pieChart-legends-color.json',
-    'option.pieChart-legends-discrete.json',
-    'option.pieChart-legends-size.json'
-  ],
-  radarChart: [
-    'option.radarChart-axes-band.json',
-    'option.radarChart-axes-linear.json',
-    'option.radarChart-legends-color.json',
-    'option.radarChart-legends-discrete.json',
-    'option.radarChart-legends-size.json'
-  ],
-  rangeAreaChart: [
-    'option.rangeAreaChart-axes-band.json',
-    'option.rangeAreaChart-axes-linear.json',
-    'option.rangeAreaChart-axes-log.json',
-    'option.rangeAreaChart-axes-symlog.json',
-    'option.rangeAreaChart-axes-time.json',
-    'option.rangeAreaChart-legends-color.json',
-    'option.rangeAreaChart-legends-discrete.json',
-    'option.rangeAreaChart-legends-size.json'
-  ],
-  rangeColumnChart: [
-    'option.rangeColumnChart-axes-band.json',
-    'option.rangeColumnChart-axes-linear.json',
-    'option.rangeColumnChart-axes-log.json',
-    'option.rangeColumnChart-axes-symlog.json',
-    'option.rangeColumnChart-axes-time.json',
-    'option.rangeColumnChart-legends-color.json',
-    'option.rangeColumnChart-legends-discrete.json',
-    'option.rangeColumnChart-legends-size.json'
-  ],
-  roseChart: [
-    'option.roseChart-axes-band.json',
-    'option.roseChart-axes-linear.json',
-    'option.roseChart-legends-color.json',
-    'option.roseChart-legends-discrete.json',
-    'option.roseChart-legends-size.json'
-  ],
-  sankeyChart: [
-    'option.sankeyChart-axes-band.json',
-    'option.sankeyChart-axes-linear.json',
-    'option.sankeyChart-axes-log.json',
-    'option.sankeyChart-axes-symlog.json',
-    'option.sankeyChart-axes-time.json',
-    'option.sankeyChart-legends-color.json',
-    'option.sankeyChart-legends-discrete.json',
-    'option.sankeyChart-legends-size.json'
-  ],
-  scatterChart: [
-    'option.scatterChart-axes-band.json',
-    'option.scatterChart-axes-linear.json',
-    'option.scatterChart-axes-log.json',
-    'option.scatterChart-axes-symlog.json',
-    'option.scatterChart-axes-time.json',
-    'option.scatterChart-legends-color.json',
-    'option.scatterChart-legends-discrete.json',
-    'option.scatterChart-legends-size.json'
-  ],
-  sequenceChart: [
-    'option.sequenceChart-axes-linear.json',
-    'option.sequenceChart-axes-log.json',
-    'option.sequenceChart-axes-symlog.json',
-    'option.sequenceChart-axes-time.json',
-    'option.sequenceChart-legends-color.json',
-    'option.sequenceChart-legends-discrete.json',
-    'option.sequenceChart-legends-size.json',
-    'option.sequenceChart-series-bar.json',
-    'option.sequenceChart-series-dot.json',
-    'option.sequenceChart-series-link.json'
-  ],
-  sunburstChart: [
-    'option.sunburstChart-legends-color.json',
-    'option.sunburstChart-legends-discrete.json',
-    'option.sunburstChart-legends-size.json'
-  ],
-  treemapChart: [
-    'option.treemapChart-legends-color.json',
-    'option.treemapChart-legends-discrete.json',
-    'option.treemapChart-legends-size.json'
-  ],
-  vennChart: [
-    'option.vennChart-legends-color.json',
-    'option.vennChart-legends-discrete.json',
-    'option.vennChart-legends-size.json'
-  ],
-  waterfallChart: [
-    'option.waterfallChart-total-custom.json',
-    'option.waterfallChart-total-end.json',
-    'option.waterfallChart-total-field.json'
-  ]
-};
-
+# Example
+User Input：
+调整图例的布局，让两个图例行内布局
+Output：
+\`\`\`
+{"key": 'legends'}
+\`\`\``
+});
 export function QARag() {
   const [query, setQuery] = React.useState('');
-  const [topK, setTopK] = React.useState(3);
+  const [topK, setTopK] = React.useState(5);
+  const [llmTopKey, setLLmTopKey] = React.useState('');
   const [qaResult, setQAResult] = React.useState<
     {
       scores: number;
@@ -283,25 +69,21 @@ export function QARag() {
       explanation: string;
     }[]
   >([]);
-  const [docsResult, setDocsResult] = React.useState<
+  const [keyPathResult, setKeyPathResult] = React.useState<
     {
       scores: number;
       index: string;
       text: string;
       key: string;
-      validKey: string;
     }[]
   >([]);
-  const [docsCompResult, setDocsCompResult] = React.useState<
+  const [topKeyResult, setTopKeyResult] = React.useState<
     {
       scores: number;
-      index: string;
       text: string;
       key: string;
-      validKey: string;
     }[]
   >([]);
-
   const getQaResult = React.useCallback(async () => {
     return await qaRag.rawRecall({
       indexName: 'betaV1',
@@ -310,45 +92,62 @@ export function QARag() {
     });
   }, [query, topK]);
 
-  const getDocsResult = React.useCallback(async () => {
-    return await docsRag.rawRecall({
-      indexName: 'betaV1',
-      topK,
-      text: query,
-      manualChunk: true,
-      dslQuery: {
-        filter: {
-          op: 'must',
-          field: 'type',
-          conds: ['barChart']
-        }
-      }
-    });
-  }, [query, topK]);
+  const getKeyPathResult = React.useCallback(
+    async (chartList: string[] = []) => {
+      return await keyPathRag.rawRecall({
+        indexName: 'betaV1',
+        topK,
+        text: query,
+        manualChunk: true,
+        dslQuery: {
+          op: 'and',
+          conds: [
+            { op: 'must', field: 'supportBar', conds: ['1'] },
+            {
+              op: 'must',
+              field: 'chartType',
+              conds: ['bar', ...chartList]
+            }
+          ]
+        },
+        sparseLogitAlpha: 0.2
+      });
+    },
+    [query, topK]
+  );
 
-  const getDocsCompResult = React.useCallback(async () => {
-    return await docsRag.rawRecall({
+  const getTopKeyResult = React.useCallback(async () => {
+    return await keyPathRag.rawRecall({
       indexName: 'betaV1',
       topK,
       text: query,
       manualChunk: true,
       dslQuery: {
-        filter: {
-          op: 'must',
-          field: 'file_name',
-          conds: chartFileMapping.barChart
-        }
-      }
+        op: 'and',
+        conds: [
+          { op: 'must', field: 'supportBar', conds: ['1'] },
+          {
+            op: 'must',
+            field: 'isTopKey',
+            conds: ['1']
+          }
+        ]
+      },
+      sparseLogitAlpha: 0.2
     });
   }, [query, topK]);
 
   const handleQuery = React.useCallback(async () => {
-    const [qaResult, docsResult, docCompResult] = await Promise.all([
-      getQaResult(),
-      getDocsResult(),
-      getDocsCompResult()
-    ]);
-    if (!qaResult?.error) {
+    const [qaResult, topKeyRes] = await Promise.all([getQaResult(), getTopKeyResult()]);
+    const llmTopKeyRes = await topKeyAtom.run({
+      query
+    });
+    if (llmTopKeyRes && !llmTopKeyRes?.error) {
+      setLLmTopKey(llmTopKeyRes?.key);
+    } else {
+      setLLmTopKey('');
+    }
+    if (qaResult && !qaResult?.error) {
       setQAResult(
         (qaResult?.result ?? []).map(item => {
           const attrs = isString(item.attrs) ? JSON.parse(item.attrs) : item.attrs;
@@ -365,41 +164,42 @@ export function QARag() {
     } else {
       setQAResult([]);
     }
-    if (!docsResult?.error) {
-      setDocsResult(
-        (docsResult?.result ?? []).map(item => {
-          const attrs = isString(item.attrs) ? JSON.parse(item.attrs) : item.attrs;
-          const { key, content, validKey, index } = attrs;
-          return {
-            scores: item.scores,
-            index: index,
-            text: content,
-            key,
-            validKey
-          };
-        })
+    if (topKeyRes && !topKeyRes?.error) {
+      const validKeys = (topKeyRes?.result || [])?.map(item => {
+        const attrs = isString(item.attrs) ? JSON.parse(item.attrs) : item.attrs;
+        const { keyPath, content } = attrs;
+        return {
+          scores: item.scores,
+          text: content,
+          key: keyPath
+        };
+      });
+      setTopKeyResult(validKeys);
+      const keyPathRes = await getKeyPathResult(
+        uniqArray([...validKeys.slice(0, 2).map(v => v.key), llmTopKeyRes?.key].filter(v => !!v))
       );
+      if (keyPathRes && !keyPathRes?.error) {
+        setKeyPathResult(
+          (keyPathRes?.result ?? []).map(item => {
+            const attrs = isString(item.attrs) ? JSON.parse(item.attrs) : item.attrs;
+            const { keyPath, content, index } = attrs;
+            return {
+              scores: item.scores,
+              index: index,
+              text: content,
+              key: keyPath
+              // validKey
+            };
+          })
+        );
+      } else {
+        setKeyPathResult([]);
+      }
     } else {
-      setDocsResult([]);
+      setTopKeyResult([]);
+      setKeyPathResult([]);
     }
-    if (!docCompResult?.error) {
-      setDocsCompResult(
-        (docCompResult?.result ?? []).map(item => {
-          const attrs = isString(item.attrs) ? JSON.parse(item.attrs) : item.attrs;
-          const { key, content, validKey, index } = attrs;
-          return {
-            scores: item.scores,
-            index: index,
-            text: content,
-            key,
-            validKey
-          };
-        })
-      );
-    } else {
-      setDocsCompResult([]);
-    }
-  }, [getDocsCompResult, getDocsResult, getQaResult]);
+  }, [getKeyPathResult, getQaResult, getTopKeyResult, query]);
 
   return (
     <div style={{ padding: 20, paddingTop: 0 }}>
@@ -413,6 +213,7 @@ export function QARag() {
         Get Rag Result
       </Button>
       <div>
+        <div>LLM Result: Key: {llmTopKey}</div>
         <div className="recall-content">
           <div className="one-content">
             <div>QA Recall:</div>
@@ -438,8 +239,8 @@ export function QARag() {
             ))}
           </div>
           <div className="one-content">
-            <div>Docs Recall:</div>
-            {docsResult.map((item, index) => (
+            <div>Top Key Recall:</div>
+            {topKeyResult.map((item, index) => (
               <Card key={index} className="qa-card">
                 <div className="qa-div">
                   <span className="title">Score:</span>
@@ -452,17 +253,13 @@ export function QARag() {
                 <div className="qa-div">
                   <div className="title">key:</div>
                   <span>{item.key}</span>
-                </div>
-                <div className="qa-div">
-                  <div className="title">validKey:</div>
-                  <span>{item.validKey}</span>
                 </div>
               </Card>
             ))}
           </div>
           <div className="one-content">
-            <div>Docs Comp Recall:</div>
-            {docsCompResult.map((item, index) => (
+            <div>KeyPath Recall:</div>
+            {keyPathResult.map((item, index) => (
               <Card key={index} className="qa-card">
                 <div className="qa-div">
                   <span className="title">Score:</span>
@@ -475,10 +272,6 @@ export function QARag() {
                 <div className="qa-div">
                   <div className="title">key:</div>
                   <span>{item.key}</span>
-                </div>
-                <div className="qa-div">
-                  <div className="title">validKey:</div>
-                  <span>{item.validKey}</span>
                 </div>
               </Card>
             ))}
--- packages/vmind/__tests__/experiment/src/pages/ChartDialogueQA/test.tsx
+++ packages/vmind/__tests__/experiment/src/pages/ChartDialogueQA/test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import React from 'react';
@@ -6,7 +7,7 @@ import { LLMManage } from '../../../../../src/index';
 import { Button } from '@arco-design/web-react';
 import { getCurrentFormattedTime, sleep } from '../../utils';
 import { data } from '../../data/qaData';
-import { ChartQAExtraction } from '../../../../../src/atom';
+import { ChartQAExtraction, CustomPrompt } from '../../../../../src/atom';
 
 const globalVariables = (import.meta as any).env;
 const llm = new LLMManage({
@@ -20,6 +21,41 @@ const llm = new LLMManage({
 });
 const qaExtraction = new ChartQAExtraction({} as any, { llm });
 
+const customAtom = new CustomPrompt({} as any, {
+  llm,
+  promptTemplate: `# Background
+You are a data visualization expert with an in-depth understanding of graphic grammar and visualization chart libraries such as d3, echarts, and ggplot2. And you know how to use simple and straightforward language to help users understand visualization content.
+Now I have a new visualization frontend library called VChart. I have extracted all the type definitions and descriptions from the VChart documentation. Your task is to generate a new type description for this key based on the user's input of the type key and type description
+
+# Requirement
+1. Generate a one-sentence description based on user input.
+2. Directly describe its effect or purpose so that beginners can understand it instantly.
+3. The influence of the parentKey MUST be considered. For example, styling a target differs when setting styles for value labels compared to axis labels.
+4. The string content of the answer will be directly parsed using JSON.parse function, so ensure the accuracy of the format.
+5. Answer must be Chinese
+
+# User Input
+\`\`\`
+{
+"key": string; // current key to generate description
+"desc": string; // current description
+"parentKeyList"?: { // The current keyPath is deeply nested, and this is the relevant description of the parent key.
+"key": string,
+"desc": string,
+}[],
+}
+\`\`\`
+# Examples
+Input:
+\`\`\`
+{"key": 'barChart.background",
+"desc": "<p>图表背景色配置，优先级高于构造函数中的 background 配置。</p>\n<p>支持三种格式：</p>\n<ul>\n<li>颜色字符串</li>\n<li>渐变色（自 1.11.6 版本开始支持）</li>\n<li>图片配置</li>\n</ul>\n<p><strong>渐变色使用如下：</strong></p>\n<p>渐变色配置。可以在图元样式的 <code class=\"codespan\">fill</code> 和 <code class=\"codespan\">stroke</code> 等支持配置颜色的属性上配置渐变色，目前支持三种渐变配置：</p>\n<ul>\n<li>线性渐变</li>\n</ul>\n\n<ul>\n<li>径向渐变</li>\n</ul>\n\n<ul>\n<li>锥形渐变</li>\n</ul>\n\n<p><strong>图片配置结构如下：</strong></p>\n<p>使用示例:</p>\n"
+\`\`\`
+Output:
+\`\`\`
+{"desc": "背景色设置"}
+\`\`\``
+});
 function extractSpecAndSolution(markdownText: string) {
   // 提取问题标题
   const regex = /title:\s*\d+\.\s*(.+?)<\/br>/;
@@ -33,7 +69,7 @@ function extractSpecAndSolution(markdownText: string) {
   }
 
   // 提取代码块中的 JSON 对象
-  const codeBlockRegex = /```(?:javascript livedemo)?\n([\s\S]*?)```/g;
+  const codeBlockRegex = /\`\`\`(?:javascript livedemo)?\n([\s\S]*?)\`\`\`/g;
   const codeExampleSectionRegex = /## 代码示例\s*([\s\S]*?)(?=\n## |$)/;
   const sectionMatch = markdownText.match(codeExampleSectionRegex);
   let specJson = null;
@@ -74,9 +110,6 @@ export function ChartQAGenerator() {
 
     const result: any = [];
     for (let i = 0; i < data.length; i++) {
-      if (![98].includes(i)) {
-        continue;
-      }
       const { content, fileName } = data[i];
       const revisedContent = content
         .replace(/^# 问题描述(\s*)$/gm, '## 问题描述')
@@ -137,46 +170,31 @@ export function ChartQAGenerator() {
     URL.revokeObjectURL(url);
   }, []);
 
+  const handleRunOne = React.useCallback(async () => {
+    console.info('---------Run Chart QA Extraction Task!---------');
+    for (let i = 0; i < 10; i++) {
+      console.info(
+        '!!!',
+        await customAtom.run({
+          query: JSON.stringify({
+            key: 'padding.left',
+            desc:
+              `<p>左边距配置。</p>\n<p>支持直接配置数值（单位 px）、百分比（<code class=\"codespan\">%</code>）(图表视窗尺寸的百分比) 以及回调配置。回调函数定义如下：</p>\n\n<p>使用示例:</p>\n<ul>\n<li>数值：<code class=\"codespan\">45</code> 为像素值 px</li>\n<li>百分比：<code class=\"codespan\">'10%'</code> 当在水平方向时是<strong>图表视图区域</strong> <code class=\"codespan\">宽度</code> 的百分比，当在垂直方向时是<strong>图表视图区域</strong> <code class=\"codespan\">高度</code> 的百分比</li>\n<li>回调：<code class=\"codespan\">({ width, height }) =&gt; width * 0.5</code> 返回值被当做像素值 px</li>\n</ul>\n` +
+              (Math.random() * 100).toFixed(2)
+          })
+        })
+      );
+    }
+  }, []);
+
   return (
     <div style={{ padding: 20 }}>
       <Button onClick={handleRun} shape="round" type="primary" size="large">
         Run Test!
       </Button>
+      <Button onClick={handleRunOne} shape="round" type="primary" size="large">
+        Generate One Setence test
+      </Button>
     </div>
   );
 }
-
-const a = [
-  { file_name: 'option.areaChart-axes-band.json', component_type: 'axes-band' },
-  { file_name: 'option.areaChart-axes-linear.json', component_type: 'axes-linear' },
-  { file_name: 'option.areaChart-axes-log.json', component_type: 'axes-log' },
-  { file_name: 'option.areaChart-axes-symlog.json', component_type: 'axes-symlog' },
-  { file_name: 'option.areaChart-axes-time.json', component_type: 'axes-time' },
-  { file_name: 'option.areaChart-legends-color.json', component_type: 'legends-color' },
-  { file_name: 'option.areaChart-legends-discrete.json', component_type: 'legends-discrete' },
-  { file_name: 'option.areaChart-legends-size.json', component_type: 'legends-size' },
-  { file_name: 'option.commonChart-series-area.json', component_type: 'series-area' },
-  { file_name: 'option.commonChart-series-bar.json', component_type: 'series-bar' },
-  { file_name: 'option.commonChart-series-boxPlot.json', component_type: 'series-boxPlot' },
-  { file_name: 'option.commonChart-series-circlePacking.json', component_type: 'series-circlePacking' },
-  { file_name: 'option.commonChart-series-dot.json', component_type: 'series-dot' },
-  { file_name: 'option.commonChart-series-funnel.json', component_type: 'series-funnel' },
-  { file_name: 'option.commonChart-series-heatmap.json', component_type: 'series-heatmap' },
-  { file_name: 'option.commonChart-series-line.json', component_type: 'series-line' },
-  { file_name: 'option.commonChart-series-linearProgress.json', component_type: 'series-linearProgress' },
-  { file_name: 'option.commonChart-series-link.json', component_type: 'series-link' },
-  { file_name: 'option.commonChart-series-map.json', component_type: 'series-map' },
-  { file_name: 'option.commonChart-series-radar.json', component_type: 'series-radar' },
-  { file_name: 'option.commonChart-series-rangeArea.json', component_type: 'series-rangeArea' },
-  { file_name: 'option.commonChart-series-rangeColumn.json', component_type: 'series-rangeColumn' },
-  { file_name: 'option.commonChart-series-rose.json', component_type: 'series-rose' },
-  { file_name: 'option.commonChart-series-sankey.json', component_type: 'series-sankey' },
-  { file_name: 'option.commonChart-series-scatter.json', component_type: 'series-scatter' },
-  { file_name: 'option.commonChart-series-sunburst.json', component_type: 'series-sunburst' },
-  { file_name: 'option.commonChart-series-treemap.json', component_type: 'series-treemap' },
-  { file_name: 'option.commonChart-series-waterfall.json', component_type: 'series-waterfall' },
-  { file_name: 'option.commonChart-series-wordCloud.json', component_type: 'series-wordCloud' },
-  { file_name: 'option.waterfallChart-total-custom.json', component_type: 'total-custom' },
-  { file_name: 'option.waterfallChart-total-end.json', component_type: 'total-end' },
-  { file_name: 'option.waterfallChart-total-field.json', component_type: 'total-field' }
-];
