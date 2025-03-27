b81037e70ced4bccd5064444a6711ea30875a9a5
xile611 <dingling112@gmail.com>
2025-03-27 16:26:02 +0800

    fix: fix reg of date

--- packages/vmind/src/utils/common.ts
+++ packages/vmind/src/utils/common.ts
@@ -41,14 +41,34 @@ export const isValidData = (data: DataCell) => {
 };
 
 const dateFormats = [
-  { key: 'YYYY年MM月DD日', regex: /^(\d{4})年(\d{2})月(\d{2})日$/, format: '$1-$2-$3' },
-  { key: 'MM月DD日YYYY年', regex: /^(\d{2})月(\d{2})日(\d{4})年$/, format: '$3-$1-$2' },
-  { key: 'YYYY年MM月', regex: /^(\d{4})年(\d{2})月$/, format: '$1-$2' },
-  { key: 'YYYY年M月', regex: /^(\d{4})年(\d{1})月$/, format: '$1-0$2' },
-  { key: 'YYYY年', regex: /^(\d{4})年$/, format: '$1' },
-  { key: 'MM月', regex: /^(\d{2})月$/, format: '$1' },
-  { key: 'M月', regex: /^(\d{1})月$/, format: '0$1' },
-  { key: 'MM月DD日', regex: /^(\d{2})月(\d{2})日$/, format: '$1-$2' }
+  {
+    key: 'YYYY年MM月DD日',
+    regex: /^(\d{4})年(\d{1,2})月(\d{1,2})日$/,
+    format: (match: string, year: string, month: string, day: string) =>
+      `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+  },
+
+  {
+    key: 'MM月DD日YYYY年',
+    regex: /^(\d{1,2})月(\d{1,2})日(\d{4})年$/,
+    format: (match: string, month: string, day: string, year: string) =>
+      `${year}-${month.padStart(2, '0')}-${year.padStart(2, '0')}`
+  },
+
+  {
+    key: 'YYYY年MM月',
+    regex: /^(\d{4})年(\d{1,2})月$/,
+    format: (match: string, year: string, month: string) => `${year}-${month.padStart(2, '0')}`
+  },
+
+  { key: 'YYYY年', regex: /^(\d{4})年$/, format: (match: string, year: string) => `${year}` },
+
+  { key: 'MM月', regex: /^(\d{1,2})月$/, format: (match: string, month: string) => `${month.padStart(2, '0')}` },
+  {
+    key: 'MM月DD日',
+    regex: /^(\d{1,2})月(\d{1,2})日$/,
+    format: (match: string, month: string, day: string) => `${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+  }
 ];
 function isQuarterDate(date: string) {
   const patterns = [/^\d{4}-Q[1-4]$/, /^\d{4}年第[一二三四]季度$/, /^Q[1-4]$/, /^\d{4}年-Q[1-4]$/];
@@ -59,7 +79,7 @@ export function validateDate(date: any) {
   //check if the string is a data string
   return (
     new Date(formaterDate).toString() !== 'Invalid Date' ||
-    dateFormats.find(v => new Date(formaterDate.replace(v.regex, v.format)).toString() !== 'Invalid Date') ||
+    !!dateFormats.find(v => new Date(formaterDate.replace(v.regex, v.format)).toString() !== 'Invalid Date') ||
     isQuarterDate(formaterDate)
   );
 }
