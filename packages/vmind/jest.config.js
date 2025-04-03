0618ce5a98a89079a10c23edd0305f14dd2fc150
ZJU_czx <952370295@qq.com>
2025-04-03 10:23:59 +0800

    feat: update jest config

--- packages/vmind/jest.config.js
+++ packages/vmind/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ...baseJestConfig,
   moduleNameMapper: {
     ...baseJestConfig.moduleNameMapper,
-    '@visactor/calculator': path.resolve(__dirname, '../calculator/src')
+    '@visactor/calculator': path.resolve(__dirname, '../calculator/src'),
+    '@visactor/chart-advisor': path.resolve(__dirname, '../chart-advisor/src')
   }
 };
