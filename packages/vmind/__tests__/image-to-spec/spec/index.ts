f984a33bb539c2038dcbc80928d46f180cc16a67
xile611 <dingling112@gmail.com>
2025-04-25 15:51:31 +0800

    test: use ts to write test case of image-to-spec

---
+++ packages/vmind/__tests__/image-to-spec/spec/index.ts
@@ -0,0 +1,140 @@
+import * as areaBasic from './area-basis';
+import * as areaPercent from './area-percent';
+
+import * as barBasic from './bar-basic';
+import * as barBiDirection from './bar-bi-direction';
+import * as barColorful from './bar-colorful';
+import * as barGradient from './bar-gradient';
+import * as barGroup from './bar-group';
+import * as barHGroup from './bar-h-group';
+import * as barHStack from './bar-h-stack';
+import * as barHorizontal from './bar-horizontal';
+import * as barPercent from './bar-percent';
+import * as barStack from './bar-stack';
+import * as barTwoDirections from './bar-two-directions';
+
+import * as circlePackingBubble from './circle-packing-bubble';
+import * as circlePackingSimple from './circle-packing-simple';
+
+import * as circularProgress from './circular-progress';
+
+import * as dualAxis from './dual-axis';
+
+import * as funnelBasic from './funnel-basic';
+import * as funnelComparative from './funnel-comparative';
+import * as funnelRect from './funnel-rect';
+import * as funnelTransform from './funnel-transform';
+
+import * as gaugeBasic from './gauge-basic';
+import * as gaugeSegment from './gauge-segment';
+
+import * as heatmapBasic from './heatmap-basic';
+
+import * as lineBasic from './line-basic';
+import * as lineStack from './line-stack';
+import * as lineBar from './line-bar';
+import * as lineMarkArea from './line-mark-area';
+import * as lineMarkline from './line-markline';
+import * as lineMultiMarkline from './line-multi-markline';
+import * as linearProgressSegment from './linear-grogress-segment';
+import * as linearProgress from './linear-progress';
+import * as liquidBasic from './liquid-basic';
+import * as liquidStar from './liquid-star';
+
+// 新增的图表引入
+import * as histogramBasic from './histogram-basic';
+import * as histogramDifferentBin from './histogram-different-bin';
+import * as pieBasic from './pie-basic';
+import * as pieDonut from './pie-donut';
+import * as pieNested from './pie-nested';
+import * as radarBasic from './radar-basic';
+import * as radarGroup from './radar-group';
+import * as radarInnerradius from './radar-innerradius';
+import * as radarStack from './radar-stack';
+import * as rangeBar from './range-bar';
+import * as rangeColumn from './range-column';
+import * as roseBasis from './rose-basis';
+import * as roseGroupStack from './rose-group-stack';
+import * as roseGroup from './rose-group';
+import * as sankeySimple from './sankey-simple';
+import * as scatterBasic from './scatter-basic';
+import * as scatterBubble from './scatter-bubble';
+import * as scatterMarkline from './scatter-markline';
+import * as sunburstBasic from './sunburst-basic';
+import * as treemap from './treemap';
+import * as vennHollow from './venn-hollow';
+import * as vennThree from './venn-three';
+import * as waterfallCollect from './waterfall-collect';
+import * as waterfallH from './waterfall-h';
+import * as waterfallStack from './waterfall-stack';
+import * as waterfall from './waterfall';
+import * as wordcloudBasic from './wordcloud-basic';
+import * as roseStack from './rose-stack';
+
+export {
+  // 面积图
+  areaBasic,
+  areaPercent,
+  barBasic,
+  barBiDirection,
+  barColorful,
+  barGradient,
+  barGroup,
+  barHGroup,
+  barHStack,
+  barHorizontal,
+  barPercent,
+  barStack,
+  barTwoDirections,
+  circlePackingBubble,
+  circlePackingSimple,
+  circularProgress,
+  dualAxis,
+  funnelBasic,
+  funnelComparative,
+  funnelRect,
+  funnelTransform,
+  gaugeBasic,
+  gaugeSegment,
+  heatmapBasic,
+  lineBasic,
+  lineStack,
+  lineBar,
+  lineMarkArea,
+  lineMarkline,
+  lineMultiMarkline,
+  linearProgressSegment,
+  linearProgress,
+  liquidBasic,
+  liquidStar,
+
+  // 新增的图表导出
+  histogramBasic,
+  histogramDifferentBin,
+  pieBasic,
+  pieDonut,
+  pieNested,
+  radarBasic,
+  radarGroup,
+  radarInnerradius,
+  radarStack,
+  rangeBar,
+  rangeColumn,
+  roseBasis,
+  roseGroupStack,
+  roseGroup,
+  sankeySimple,
+  scatterBasic,
+  scatterBubble,
+  scatterMarkline,
+  sunburstBasic,
+  treemap,
+  vennHollow,
+  vennThree,
+  waterfallCollect,
+  waterfallH,
+  waterfallStack,
+  waterfall,
+  wordcloudBasic,
+  roseStack
+};
---
+++ packages/vmind/__tests__/image-to-spec/spec/radar-group.ts
@@ -0,0 +1,55 @@
+const mockData = [];
+const types = ['A', 'B', 'C'];
+
+types.forEach(type => {
+  for (let i = 1; i <= 12; i++) {
+    mockData.push({ month: i + 'th', value: Math.random() * 100 + 10, type });
+  }
+});
+
+const spec = {
+  type: 'radar',
+  data: [
+    {
+      values: mockData
+    }
+  ],
+  categoryField: 'month',
+  valueField: 'value',
+  seriesField: 'type',
+  axes: [
+    {
+      orient: 'radius',
+      grid: {
+        smooth: true, // smooth grid lines
+        style: {
+          lineDash: [0]
+        },
+        alternateColor: '#f5f5f5' // Configure the background color between grid lines
+      }
+    },
+    {
+      orient: 'angle',
+      tick: {
+        visible: false
+      },
+      domainLine: {
+        visible: true,
+        style: {
+          stroke: '#333'
+        }
+      },
+      grid: {
+        style: {
+          lineDash: [0]
+        }
+      }
+    }
+  ],
+  legends: {
+    visible: true,
+    orient: 'top'
+  }
+};
+
+export { spec };
